1f0e3629e0f55746128a975c43671bd511e43fb5
bchoquepoquechoque <crash_chh@hotmail.com>
2021-09-09 12:57:01 -0400

    Agregar archivos de proyecto.

---
+++ Projeco01/Principal.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Datos Personales : Choque Poquechoque Brayan Erick" << std::endl;
+	std::cout << "carrera : ingenieria en sistemas " << std::endl;
+	std::cout << "CU: 35-5222" << std::endl;
+	std::cout << "Email: brayanyale0@gmail.com" << std::endl;
+	return 0;
+}
